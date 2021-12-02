78996ac411896a382d6f90def0e38be9a3d82c73
sanekSOkOlOv <oleksandr.sokolov@nure.ua>
2021-12-02 14:24:32 +0200

    Тестовый консольный проект

---
+++ Tests/WPFConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPFConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
